9d983b68697fc3f7b2d3465e402fe218d138da02
Federico Marini <marinif@uni-mainz.de>
2024-04-05 16:30:15 +0200

    defining a proper show method for the SCAE objects

---
+++ R/SingleCellAlleleExperiment-methods.R
@@ -0,0 +1,48 @@
+#' @name SingleCellAlleleExperiment-misc
+#'
+#' @title Miscellaneous SingleCellAlleleExperiment methods
+#'
+#' @description
+#' Miscellaneous methods for the \code{\link{SingleCellAlleleExperiment}} class 
+#' and its descendants that do not fit into any other documentation category 
+#' such as, for example, show methods.
+#'
+#' @param object a \code{\link{SingleCellAlleleExperiment}} object
+#'
+#' @return Returns NULL
+NULL
+
+
+# SingleCellExperiment show method ----------------------------------------
+
+#' @importFrom S4Vectors coolcat
+#' @importFrom methods callNextMethod
+.scae_show <- function(object) {
+  n_tot_feats <- nrow(object)
+  n_a_feats <- nrow(get_agenes(object))
+  n_ni_feats <- nrow(get_nigenes(object))
+  n_fun_feats <- nrow(scae_subset_functional(object))
+  
+  callNextMethod()
+  cat(
+   "----------\n", 
+   "Including a total of ", n_tot_feats, " features:\n",
+   sep = ""
+  )
+  coolcat("Immune genes (%d): %s\n", 
+          rownames(get_agenes(object)))
+  coolcat("Allele-level information (%d): %s\n", 
+          rownames(scae_subset_alleles(object)))
+  coolcat("Functional level information (%d): %s\n", 
+          rownames(scae_subset_functional(object)))
+  coolcat("Non-immune genes (%d): %s\n", 
+          rownames(get_nigenes(object)))
+}
+
+#' @rdname SingleCellAlleleExperiment-misc
+#' 
+#' @export
+setMethod("show",
+          signature = signature(object = "SingleCellAlleleExperiment"),
+          definition = .scae_show)
+
